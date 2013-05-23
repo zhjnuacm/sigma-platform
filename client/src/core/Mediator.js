d1ed157f06718e15facd52f6e15b1568a95f2386
Jopix <kkil58@126.com>
2013-05-23 10:35:07 +0800

    把控件整合进去

--- client/src/core/Mediator.js
+++ client/src/core/Mediator.js
@@ -1,3 +1,5 @@
+var inputD;
+var friendList;
 
 function Mediator() {
 	this._hero;
@@ -9,6 +11,8 @@ function Mediator() {
 	this._size;
 	this._tipsManage;
 	this._input;
+
+
 	this.init = function(mainLayer) {
 		this._size = cc.Director.getInstance().getWinSize();
 		// get the main layer
@@ -29,7 +33,28 @@ function Mediator() {
 
 		this._mainLayer.addChild(this._hero.getSprite());
 
+	    //
+	    //this._input = InputPanel.create();
+	    //this._mainLayer.addChild(this._input, 2);
+	    //this._input.adaptPoistion();
+		inputD = InputPanel.create();
+		this._mainLayer.addChild(inputD, 2);
+		inputD.adaptPoistion();
+
+
+		//var mes = new Message();
+		//mes.init2("我今天拉屎好高兴好开心啊感叹号呢？！有谁能分享我的喜悦。", 26);
+		//var tb = TextBox.create(mes, 1);
+		//this._mainLayer.addChild(tb);
+		//tb.setPosition(cc.p(400, 300));
+
+
+	    var friend = FriendList.create(cc.c4(236,236, 236, 255), 180, 288);
+	    friend.setPosition(cc.p(this._size.width - 200, 220));
+	    this._mainLayer.addChild(friend);
 		
+		friendList = FriendList.create();
+		this._mainLayer.addChild(friendList);
 
 		return true;
 	},
--- client/src/resource.js
+++ client/src/resource.js
@@ -6,8 +6,29 @@ var s_Search  			= "client/res/icon_1.png";
 var s_Pk  				= "client/res/icon_2.png";
 var s_Chat  			= "client/res/icon_3.png";
 var s_Fav  				= "client/res/icon_4.png";
-var s_map_res 			= "client/res/map/smapRule.png";
-var s_hero 				= "client/res/hero.png";
+var s_hero              = "client/res/hero.png";
+var s_face              = "client/res/face_normal.png";
+var s_face2             = "client/res/face_down.png";
+var s_inputR            = "client/res/InputPanel_Rect.png";
+var s_cr                = "client/res/cr.png";
+var s_openview          = "client/res/openViewButton.png";
+var s_closeview         = "client/res/closeViewButton.png";
+var s_user              = "client/res/user.png";
+var s_ScrolLine         = "client/res/scroll_line.png";
+var s_ScrolPoint        = "client/res/scroll_point.png";
+var s_TBbottonleft      = "client/res/textbox_botton_left.png";
+var s_TBbuttonright     = "client/res/textbox_botton_right.png";
+var s_TBtopleft         = "client/res/textbox_top_left.png";
+var s_TBtopright        = "client/res/textbox_top_right.png";
+var s_TBright           = "client/res/textbox_right.png";
+var s_Friend_t          = "client/res/friend_top.png";
+var s_Friend_b          = "client/res/friend_botton.png";
+var s_Friend_l          = "client/res/friend_left.png";
+var s_Friend_r          = "client/res/friend_right.png";
+var s_FriendList_t      = "client/res/friend_list_top.png";
+var s_yahei             = "Microsoft YaHei";
+var s_Friendseach       = "client/res/friend_seach.png";
+var s_map_res           = "client/res/map/smapRule.png";
 //tmx map 
 var s_smap 				= "client/res/map/map1.tmx";
 
@@ -21,6 +42,28 @@ var g_ressources = [
     {type:"image", src:s_Pk},
     {type:"image", src:s_Fav},
     {type:"image", src:s_Chat},
+
+    { type: "image", src: s_face },
+    { type: "image", src: s_face2 },
+    { type: "image", src: s_Chat },
+    { type: "image", src: s_inputR },
+    { type: "image", src: s_cr },
+    { type: "image", src: s_openview },
+    { type: "image", src: s_user },
+    { type: "image", src: s_ScrolLine },
+    { type: "image", src: s_ScrolPoint },
+    { type: "image", src: s_closeview },
+    { type: "image", src: s_TBbottonleft },
+    { type: "image", src: s_TBbuttonright },
+    { type: "image", src: s_TBtopleft },
+    { type: "image", src: s_TBtopright },
+    { type: "image", src: s_TBright },
+    { type: "image", src: s_Friend_t },
+    { type: "image", src: s_Friend_b },
+    { type: "image", src: s_Friend_r },
+    { type: "image", src: s_Friend_l },
+    { type: "image", src: s_FriendList_t },
+    { type: "image", src: s_Friendseach },
 
 
     {type:"image", src:s_map_res},
